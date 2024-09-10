2f4d362fd4ec63a8caf410e264c8dfe60ae01952
Jzdoot <9559123-jzdoot@users.noreply.gitlab.com>
2024-09-10 16:51:00 -0400

    fixing a error that came out of the merge

--- lua/jzdoot/lazy.lua
+++ lua/jzdoot/lazy.lua
@@ -58,7 +58,6 @@ require("lazy").setup({
 	{"nvim-lualine/lualine.nvim"},
 	{"ixru/nvim-markdown"},
 	{"glacambre/firenvim", build = ":call firenvim#install(0)"},
-    {import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
