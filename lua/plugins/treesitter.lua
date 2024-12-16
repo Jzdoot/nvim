4e0833904de2e003aed5fb840f5f11851454d05e
Jzdoot <9559123-jzdoot@users.noreply.gitlab.com>
2024-12-15 22:22:59 -0500

    feat: added lots of stuff like lsp, I still need to add some completion

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,20 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
+		build = ":TSUpdate",
 		config = function ()
 			require'nvim-treesitter.configs'.setup {
-				ensure_installed = {"lua","java","bash"},
+				ensure_installed = {"lua", "bash"},
 				sync_install = false,
 				auto_install = true,
 				highlight = {
-					enable = true,
-					disable = {"latex"},
-					additional_vim_regex_highlighting = {'org'},
+					disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
 				},
 				playground = {
 					enable = true,
