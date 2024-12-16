40bc4862c43b7a89a0b203116841413dc3cf9567
Jzdoot <9559123-jzdoot@users.noreply.gitlab.com>
2024-12-15 22:22:59 -0500

    feat: added lots of stuff like lsp, I still need to add some completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,10 +1,10 @@
 return {
-	{"hrsh7th/nvim-cmp"},
-	{"hrsh7th/cmp-buffer"},
-	{"hrsh7th/cmp-path"},
-	{"saadparwaiz1/cmp_luasnip"},
-	{"hrsh7th/cmp-nvim-lsp"},
-	{"hrsh7th/cmp-nvim-lua"},
-	{"L3MON4D3/LuaSnip"},
-	{"rafamadriz/friendly-snippets"},
+	-- {"hrsh7th/nvim-cmp"},
+	-- {"hrsh7th/cmp-buffer"},
+	-- {"hrsh7th/cmp-path"},
+	-- {"saadparwaiz1/cmp_luasnip"},
+	-- {"hrsh7th/cmp-nvim-lsp"},
+	-- {"hrsh7th/cmp-nvim-lua"},
+	-- {"L3MON4D3/LuaSnip"},
+	-- {"rafamadriz/friendly-snippets"},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,18 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function ()
-			require("telescope").setup{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.8',
+		dependencies = {
+			{ 'nvim-lua/plenary.nvim' },
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+		},
+		config = function()
+			require("telescope").setup {
 				defaults = {
 					mappings = {
 						i = {
-							["<c-n>"] = "move_selection_previous";
-							["<c-p>"] = "move_selection_next";
+							["<c-n>"] = "move_selection_previous",
+							["<c-p>"] = "move_selection_next",
 						}
 					},
 					pickers = {
@@ -21,9 +25,14 @@ return {
 			}
 			local tele = require("telescope.builtin")
 			vim.keymap.set("n", "<space>.", tele.find_files)
-			vim.keymap.set("n","<space>g", tele.live_grep)
-			vim.keymap.set("n","<space>i", tele.lsp_implementations)
-			vim.keymap.set("n","<leader>h", tele.help_tags)
+			vim.keymap.set("n", "<space>g", tele.live_grep)
+			vim.keymap.set("n", "<space>i", tele.lsp_implementations)
+			vim.keymap.set("n", "<space>h", tele.help_tags)
+			vim.keymap.set("n", "<space>en", function()
+				tele.find_files {
+					cwd = vim.fn.stdpath("config")
+				}
+			end)
 		end
 	},
 }
