46172786159e962b78411bbba5c1126d13e36899
Jzdoot <jacobsamurin@gmail.com>
2024-12-15 22:39:14 -0500

    fix: removed unneeded stuff

--- lua/jzdoot/lazy.lua
+++ lua/jzdoot/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
 			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
+			{ out,                            "WarningMsg" },
 			{ "\nPress any key to exit..." },
 		}, true, {})
 		vim.fn.getchar()
@@ -17,50 +17,12 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		-- {"romgrk/doom-one.vim"},
-		-- {"tpope/vim-fugitive"},
-		{"ap/vim-css-color"},
-		{"nvim-lua/plenary.nvim"},
-		-- {
-		-- 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		-- 	dependencies = { 'nvim-lua/plenary.nvim' }
-		-- },
-		-- {"mbbill/undotree"},
-		{"wakatime/vim-wakatime"},
-		-- {"ThePrimeagen/vim-be-good"},
-		{"kyazdani42/nvim-web-devicons"},
-		-- {"nvim-treesitter/nvim-treesitter" , run = ":TSUpdate"},
-		-- {"nvim-treesitter/playground"},
-		-- {
-		-- 	"ThePrimeagen/harpoon",
-		-- 	branch = "harpoon2",
-		-- 	dependencies = { "nvim-lua/plenary.nvim" }
-		-- },
-		-- {"neovim/nvim-lspconfig"},
-		-- {"VonHeikemen/lsp-zero.nvim", branch = "v4.x"},
-		-- {"williamboman/mason.nvim"},
-		-- {"williamboman/mason-lspconfig.nvim"},
-		-- {"hrsh7th/nvim-cmp"},
-		-- {"hrsh7th/cmp-buffer"},
-		-- {"hrsh7th/cmp-path"},
-		-- {"saadparwaiz1/cmp_luasnip"},
-		-- {"hrsh7th/cmp-nvim-lsp"},
-		-- {"hrsh7th/cmp-nvim-lua"},
-		-- {"L3MON4D3/LuaSnip"},
-		-- {"rafamadriz/friendly-snippets"},
-		-- {
-		-- 	"nvim-lualine/lualine.nvim",
-		-- 	dependencies = { 'nvim-tree/nvim-web-devicons' }
-		-- },
-		-- {"glacambre/firenvim", build = ":call firenvim#install(0)"},
-		-- {"paulfrische/mpd.nvim"},
-		-- {
-			-- 	"epwalsh/obsidian.nvim",
-			-- 	dependencies = { 'nvim-lua/plenary.nvim'}
-			-- }
-			{ import = "plugins" },
-		},
-		checker = { enabled = true },
-		-- import your plugins
-	}
+		{ "ap/vim-css-color" },
+		{ "nvim-lua/plenary.nvim" },
+		{ "wakatime/vim-wakatime" },
+		{ "kyazdani42/nvim-web-devicons" },
+		{ import = "plugins" },
+	},
+	checker = { enabled = true },
+}
 )
