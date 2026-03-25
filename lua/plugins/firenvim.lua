38aee6a1b70071102018ebe9a11554dade99e682
Jzdoot <jacobsamurin@gmail.com>
2026-03-25 13:17:04 -0400

    fixed things

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -4,62 +4,26 @@ return {
 		build = ":call firenvim#install(0)",
 		config = function()
 			vim.g.firenvim_config = {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-				globalSettings = { alt = "all" },
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 				globalSettings = {
 					alt = "all",
 					takeover = "once"
 				},
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 				localsettings = {
 					[".*"] = {
 						cmdline  = "neovim",
 						content  = "text",
 						priority = 0,
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-						-- selector	= "textarea",
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 						takeover = "never"
 					},
 					["https://replit.com/"] = {
 						priority = 1,
 						takeover = "never"
 					},
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 					["https://learn.zybooks.com/"] = {
-=======
-					["aleks.com"] = {
->>>>>>> Stashed changes
-=======
-					["aleks.com"] = {
->>>>>>> Stashed changes
-=======
-					["aleks.com"] = {
->>>>>>> Stashed changes
-						priority = 1,
-						takeover = "never"
+						["aleks.com"] = {
+							priority = 1,
+							takeover = "never"
+						}
 					}
 				}
 			}
